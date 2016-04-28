397c90c8ffd08ba2320043ab5f31c50d4ef2e48e
James Hunt <james@niftylogic.com>
2016-04-28 09:45:31 -0400

    Introduce Deployer

---
+++ deployer.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Deployer struct {
+	manifest *Manifest
+}
+
+func (d *Deployer) Deploy() {
+	for _, domain := range d.manifest.Domains {
+		fmt.Printf("setting up global domain '%s'\n", domain)
+	}
+	for name, org := range d.manifest.Organizations {
+		fmt.Printf("creating organization '%s'\n", name)
+		for _, domain := range org.Domains {
+			fmt.Printf("  setting up organization domain '%s'\n", domain)
+		}
+		for name, roles := range org.Users {
+			fmt.Printf("  granting org-level access to user '%s'\n", name)
+			for _, role := range roles {
+				fmt.Printf("    granting role '%s' to %s\n", role, name)
+			}
+		}
+		for name, space := range org.Spaces {
+			fmt.Printf("  creating space '%s'\n", name)
+			fmt.Printf("    setting ssh-enabled to '%s'\n", space.SSH)
+			fmt.Printf("    using default domain of '%s'\n", space.Domain)
+			for name, roles := range space.Users {
+				fmt.Printf("    granting space-level access to user '%s'\n", name)
+				for _, role := range roles {
+					fmt.Printf("      granting role '%s' to %s\n", role, name)
+				}
+			}
+			for name, service := range space.SharedServices {
+				fmt.Printf("    setting up shared service instance '%s' (from %s)\n", name, service)
+			}
+			for _, app := range space.Applications {
+				fmt.Printf("    deploying application '%s'\n", app.Name)
+				fmt.Printf("      spinning up %d instances\n", app.Instances)
+				if app.Domain != "" {
+					fmt.Printf("      using domain '%s'\n", app.Domain)
+				}
+				if app.Disk != "" {
+					fmt.Printf("      provisioning with %s disk\n", app.Disk)
+				}
+				if app.Memory != "" {
+					fmt.Printf("      provisioning with %s memory\n", app.Memory)
+				}
+				if app.Image != "" {
+					fmt.Printf("      deploying image '%s'\n", app.Image)
+				} else if app.Repository != "" {
+					fmt.Printf("      deploying remote codebase from '%s'\n", app.Repository)
+				} else if app.Path != "" {
+					fmt.Printf("      deploying local codebase from '%s'\n", app.Path)
+				}
+
+				for name, service := range app.BoundServices {
+					fmt.Printf("      binding service instance '%s' (from %s)\n", name, service)
+				}
+			}
+		}
+	}
+}
